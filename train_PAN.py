cca8741d8a6a175fa9590bddfeaf7ea4cea6e5b3
WaBiKong <ricardo2080355415@gmail.com>
2024-02-26 00:47:14 +0800

    add train result unet,pspnet,PAN

--- train_PAN.py
+++ train_PAN.py
@@ -20,7 +20,7 @@ def parser_args():
 
     parser.add_argument('--workers', default=8, type=int, metavar='N',
                         help='number of data loading workers')
-    parser.add_argument('--epochs', default=200, type=int, metavar='N',
+    parser.add_argument('--epochs', default=400, type=int, metavar='N',
                         help='number of total epochs to run')
     parser.add_argument('--lr', default=1e-4, type=float,
                         metavar='LR', help='initial learning rate', dest='lr')
--- train_Unet.py
+++ train_Unet.py
@@ -22,7 +22,7 @@ def parser_args():
 
     parser.add_argument('--workers', default=8, type=int, metavar='N',
                         help='number of data loading workers')
-    parser.add_argument('--epochs', default=200, type=int, metavar='N',
+    parser.add_argument('--epochs', default=400, type=int, metavar='N',
                         help='number of total epochs to run')
     parser.add_argument('--lr', default=1e-3, type=float,
                         metavar='LR', help='initial learning rate', dest='lr')
