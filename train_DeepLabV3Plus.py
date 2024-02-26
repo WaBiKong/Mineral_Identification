213c6a5657692f6cda32b2472bfde329f9e19b04
WaBiKong <ricardo2080355415@gmail.com>
2024-02-26 23:28:44 +0800

    add train result deeplabv3+

--- train_DeepLabV3Plus.py
+++ train_DeepLabV3Plus.py
@@ -9,9 +9,9 @@ from seg_utils.seg_train_utils import evaluate as validate
 
 def parser_args():
 
-    parser = argparse.ArgumentParser(description="train Unet")
+    parser = argparse.ArgumentParser(description="train DeepLab")
 
-    parser.add_argument('--batch_size', default=16, type=int)
+    parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--img_size', default=384, type=int,
                         help="size of input images")
     
@@ -20,7 +20,7 @@ def parser_args():
 
     parser.add_argument('--workers', default=8, type=int, metavar='N',
                         help='number of data loading workers')
-    parser.add_argument('--epochs', default=200, type=int, metavar='N',
+    parser.add_argument('--epochs', default=400, type=int, metavar='N',
                         help='number of total epochs to run')
     parser.add_argument('--lr', default=1e-4, type=float,
                         metavar='LR', help='initial learning rate', dest='lr')
